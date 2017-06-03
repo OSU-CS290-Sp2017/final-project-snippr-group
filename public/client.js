ec2f61ddd60788efc5fcf399bd32171aa41b69e1
nickm <nickm@outofthisworld.net>
2017-06-03 11:26:54 -0700

    styles now cached on client side less server strain = good

--- public/client.js
+++ public/client.js
@@ -1,11 +1,14 @@
 var styleSelect = function() {return document.getElementById('styleSelect-menu');};
 
+var cacheStyles = {};
+
 function loadStyle(styleName){
     var xhttp = new XMLHttpRequest();
 
     xhttp.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
             console.log('loaded', styleName);
+            cacheStyles[styleName] = this.responseText;
             document.getElementById('highlight-style').innerHTML = this.responseText;
         }
     }
@@ -17,7 +20,18 @@ function loadStyle(styleName){
 function loadEvent(){
     var style = styleSelect().value;
     console.log('loading', style);
-    loadStyle(style);
+    getStyle(style);
+}
+
+function getStyle(styleName){
+    if(Object.keys(cacheStyles).indexOf(styleName) === -1){
+        console.log('retrieving', styleName);
+        loadStyle(styleName);
+    }
+    else{
+        console.log('from cache', styleName);
+        document.getElementById('highlight-style').innerHTML = cacheStyles[styleName];
+    }
 }
 
 loadEvent();
